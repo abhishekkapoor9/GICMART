9e106bfcc124f642ea5382b7f38a20e06a3e7af8
Abhishek kapoor <abhishekkpr9@gmail.com>
2017-11-14 22:03:16 +0530

    rttrt

--- gicmart/Controllers/registrationController.cs
+++ gicmart/Controllers/registrationController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using gicmart.Models;
+
 
 namespace gicmart.Controllers
 {
@@ -10,15 +15,50 @@ namespace gicmart.Controllers
     {
         //
         // GET: /registration/
+        public ActionResult register()
+        {
+            return View();
+        }
 
-        public string registrationmethod()
+        //public string registrationmethod()
+        //{
+        //    return "welcome to registration ";
+        //}
+        [HttpPost]
+        public ActionResult register( users usr)
         {
-            return "welcome to registration ";
+            if (ModelState.IsValid)
+            {
+                string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+                SqlConnection con = new SqlConnection(cs);
+                con.Open();
+                string sp = "registration_sp";
+                SqlCommand cmd = new SqlCommand("sp", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@userid", usr.userid);
+                cmd.Parameters.AddWithValue("@address", usr.address);
+                cmd.Parameters.AddWithValue("@password", usr.password);
+                cmd.Parameters.AddWithValue("@sponsorid", usr.sponsorid);
+                cmd.Parameters.AddWithValue("@sponsorname", usr.sponsorname);
+                cmd.Parameters.AddWithValue("@id", usr.pin);
+                cmd.Parameters.AddWithValue("@name", usr.name);
+                cmd.Parameters.AddWithValue("@nomineename", usr.nomineename);
+                cmd.Parameters.AddWithValue("@state", usr.state);
+                cmd.Parameters.AddWithValue("@city", usr.city);
+                cmd.Parameters.AddWithValue("@pancardno", usr.pancardno);
+                cmd.Parameters.AddWithValue("@mobileno", usr.mobileno);
+                cmd.Parameters.AddWithValue("@terandcontion", usr.termandcondition);
+                int wq = cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            return View(usr);
         }
-        public ActionResult registration()
-        {
-            return View("registration");
-        }
+        
+        //public ActionResult registration()
+        //{
+            
+        //    return View("registration");
+        //}
 
     }
 }
---
+++ gicmart/Models/users.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace gicmart.Models
+{
+    public class users
+    {
+        [Display(Name = "please enter user id")]
+        [Required]
+        public string userid { get; set; }
+        [Display(Name = " please enter sponsor id")]
+        [Required]
+        public string sponsorid { get; set; }
+        [Display(Name = "please enter sponsor name")]
+        [Required]
+        public string sponsorname { get; set; }
+        [Display(Name = "please enter pin")]
+        [Required]
+        public string pin { get; set; }
+        [Display(Name = "please enter name")]
+        [Required]
+        public string name { get; set; }
+        public int mobileno { get; set; }
+        [Display(Name = "please enter password")]
+        [Required]
+        public string password { get; set; } 
+        public string pancardno { get; set; }
+        public string nomineename { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+        [Required]
+        public bool termandcondition { get; set; }
+ 
+    }
+}
