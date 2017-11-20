9591103830d8d033d244734834b57b1f439b505b
Abhishek Kapoor <abhishekkpr9@gmail.com>
2017-11-20 17:32:37 +0530

    login with tempdata

--- gicmart/Controllers/contactusController.cs
+++ gicmart/Controllers/contactusController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Configuration;
 using System.Net.Mail;
-using gicmart.Models.contactus;
+
 
 
 namespace gicmart.Controllers
@@ -26,22 +26,22 @@ namespace gicmart.Controllers
             return View("contactus");
         }
          [HttpPost]  
-        public ViewResult contactus(contactus.Models.contactus obj) {  
+        public ViewResult contactus(gicmart.Models.contactus obj) {  
             if (ModelState.IsValid) {  
-                MailMessage mail = new MailMessage();  
-                mail.To.Add(obj.To);  
-                mail.From = new MailAddress(obj.From);  
-                mail.Subject = obj.Subject;  
-                string Body = obj.Body;  
-                mail.Body = Body;  
-                mail.IsBodyHtml = true;  
-                SmtpClient smtp = new SmtpClient();  
-                smtp.Host = "smtp.gmail.com";  
-                smtp.Port = 587;  
-                smtp.UseDefaultCredentials = false;  
-                smtp.Credentials = new System.Net.NetworkCredential("rk.singh9646@gmail.com", "10904070"); // Enter seders User name and password  
-                smtp.EnableSsl = true;  
-                smtp.Send(mail);  
+                //MailMessage mail = new MailMessage();  
+                //mail.To.Add(obj.To);  
+                //mail.From = new MailAddress(obj.From);  
+                //mail.Subject = obj.Subject;  
+                //string Body = obj.Body;  
+                //mail.Body = Body;  
+                //mail.IsBodyHtml = true;  
+                //SmtpClient smtp = new SmtpClient();  
+                //smtp.Host = "smtp.gmail.com";  
+                //smtp.Port = 587;  
+                //smtp.UseDefaultCredentials = false;  
+                //smtp.Credentials = new System.Net.NetworkCredential("rk.singh9646@gmail.com", "10904070"); // Enter seders User name and password  
+                //smtp.EnableSsl = true;  
+                //smtp.Send(mail);  
                 return View("contactus", obj);  
             } else {  
                 return View();  
