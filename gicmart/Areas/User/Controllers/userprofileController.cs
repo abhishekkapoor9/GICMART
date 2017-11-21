0ab22d7d374cb4d26bc55d3f2b04ce6797f0ece1
Abhishek Kapoor <abhishekkpr9@gmail.com>
2017-11-21 16:48:16 +0530

    profile page

--- gicmart/Areas/User/Controllers/userprofileController.cs
+++ gicmart/Areas/User/Controllers/userprofileController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using gicmart.Models;
+using gicmart.Areas.User.Models;
 
 namespace gicmart.Areas.User.Controllers
 {
@@ -14,11 +18,44 @@ namespace gicmart.Areas.User.Controllers
 
         public ActionResult Index()
         {
+            List<userprofile> imagelst = new List<userprofile>();
+            SqlDataReader rdr = null;
+            var profileinfo =new userprofile();
+
             TempData["userId"] = System.Web.HttpContext.Current.Session["userId"];
             TempData["userName"] = System.Web.HttpContext.Current.Session["userName"];
-            ViewBag.sectionName = "Profile";
-            return View();
-        }
+            ViewBag.sectionName = "Dashboard";
+            string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            string usersp2 = "sp_ProfileInfo";
+            SqlCommand cmd4 = new SqlCommand(usersp2, con);
+            cmd4.CommandType = CommandType.StoredProcedure;
 
+            cmd4.Parameters.AddWithValue("@empid", System.Web.HttpContext.Current.Session["userId"]);
+            //cmd4.ExecuteNonQuery(); // MISSING
+            //getting reference_user_Id
+            try
+            {
+                rdr = cmd4.ExecuteReader();
+                // iterate through results, printing each to console
+                while (rdr.Read())
+                {
+                    profileinfo = new userprofile
+                    {
+                        sponsorid = rdr["sponsor_id"].ToString()
+                    };
+                }
+                rdr.Close();
+            }
+            catch (Exception e1)
+            {
+                //reference_user_id = null;
+            }
+           
+            return View(profileinfo);
+        }
     }
+
 }
+
